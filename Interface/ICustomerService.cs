f7e155e265ca72aab2f2138d70cb15880cf26fdc
Olatunji Yusuf <olatunjiyso@gmail.com>
2021-09-18 11:03:35 +0100

    create interfaces for customer operations

---
+++ Interface/ICustomerService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using ShopMart.DTO;
+
+namespace ShopMart.Interface
+{
+   public interface ICustomerService
+    {
+      public  AuthenticationResponse Authenticate(AuthenticationRequest request);
+      public  RegistrationResponse Register(RegistrationRequest request);
+      public  IEnumerable<CustomerDTO> GetAll();
+      public  CustomerDTO GetById(long id);
+      public CustomerDTO UpdateCustomer(long id, UpdateCustomerRequest request);
+      public void DeleteCustomer(long id);
+    }
+}
---
+++ Interface/IJwtUtils.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using ShopMart.DTO;
+using ShopMart.Models;
+
+namespace ShopMart.Interface
+{
+    public interface IJwtUtils
+    {
+        public string GenerateToken(Customer customer);
+        public EncodedUser? ValidateToken(string token);
+    }
+}
